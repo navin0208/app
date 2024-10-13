15bb37a1b8db27e031bb4561f3a3485912a9654b
navin0208 <143115534+navin0208@users.noreply.github.com>
2024-10-13 13:34:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,16 @@
-from flask import Flask, send_from_directory, render_template, jsonify, request, redirect, url_for, flash, session
+from flask import Flask, render_template, jsonify, request
 from flask_socketio import SocketIO, emit
-import pandas as pd
-import os
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 socketio = SocketIO(app)  # Initialize SocketIO
 
-users = {
-    "admin": "decon123"
-}
-
+# Store data from Raspberry Pi
 pi_data = {}
 
 @app.route('/')
-def home():
-    return render_template('login.html')
-
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-    
-    if username in users and users[username] == password:
-        session['username'] = username
-        return redirect(url_for('protected'))
-    else:
-        flash('Wrong Password or Username')
-        return redirect(url_for('home'))
-
-@app.route('/protected')
-def protected():
-    if 'username' in session:
-        return render_template('index.html', username=session['username'])
-    return redirect(url_for('home'))
+def index():
+    return render_template('index.html')
 
 # Endpoint to receive data from Raspberry Pis
 @app.route('/send_data', methods=['POST'])
@@ -44,7 +21,7 @@ def receive_data():
     not_ok_count = data.get('not_ok_count')
     shift = data.get('shift')
 
-    # Store the data received from the Raspberry Pi
+    # Store the data received from Raspberry Pi
     pi_data[pi_id] = {
         'product_count': product_count,
         'not_ok_count': not_ok_count,
@@ -56,25 +33,11 @@ def receive_data():
 
     return jsonify({'status': f'Data received from {pi_id}'})
 
+# Serve the stored data to the frontend
 @app.route('/get_data', methods=['GET'])
 def get_data():
     return jsonify(pi_data)
 
-@app.route('/download_data', methods=['GET'])
-def download_data():
-    df = pd.DataFrame.from_dict(pi_data, orient='index')
-    file_path = 'raspberry_pi_data.xlsx'
-    df.to_excel(file_path, index=True)
-    return send_from_directory(os.getcwd(), file_path, as_attachment=True)
-
-@app.route('/Images/<path:filename>')
-def serve_image(filename):
-    return send_from_directory('Images', filename)
-
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('home'))
-
+# Run the Flask-SocketIO app
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
